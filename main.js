508bf76a18c1e8c5551a8814e554980c473dd877
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2023-03-17 22:12:12 -0400

    Adicionar funcao Comparar-valores

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-const form = document.getElementById('form-validacao');
+const form = document.getElementById('form-validacao');//constante para remover a necessidade de update do navegador
+const mensagem = document.getElementById('mensagem');
+
+form.addEventListener('submit', function(e) { //sera disparado quando um evento do tipo 'submit' acontecer no form
+    const campoA = parseInt(document.getElementById('campo-a').value); //parseInt converte para numero intero
+    const campoB = parseInt(document.getElementById('campo-b').value);
+    e.preventDefault(); // interrompe o envio do formulário
+    
+    if (campoB <= campoA) {
+    mensagem.textContent = 'Erro! O Valor B deve ser maior que o Valor A!';
+    mensagem.classList.add('erro');
+    } else {
+        mensagem.textContent = 'Formulário enviado com sucesso!';
+        mensagem.classList.add('sucesso');
+    }
+});
+console.log(form); //mostrar no console, a constante
