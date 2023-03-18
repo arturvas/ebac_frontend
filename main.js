47c23f8a73c451c171f0d8817883f904cada0cf5
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2023-03-18 01:05:21 -0400

    adicionado suposta melhoria na lógica

--- main.js
+++ main.js
@@ -4,16 +4,18 @@ const mensagem = document.getElementById('mensagem');
 form.addEventListener('submit', function(e) { //sera disparado quando um evento do tipo 'submit' acontecer no form
     const campoA = parseInt(document.getElementById('campo-a').value); //parseInt converte para numero intero
     const campoB = parseInt(document.getElementById('campo-b').value);
-    e.preventDefault(); // interrompe o envio do formulário
     
     if (campoA <= campoB) {
         mensagem.textContent = 'Formulário enviado com sucesso!';
         mensagem.classList.add('sucesso');
         mensagem.classList.remove('erro');
+        e.preventDefault();
     } else {
         mensagem.textContent = 'Erro! O Valor B deve ser maior que o Valor A!';
         mensagem.classList.add('erro');
         mensagem.classList.add('sucesso');
+        e.preventDefault(); // interrompe o envio do formulário
+
     }
 });
 console.log(form); //mostrar no console, a constante
