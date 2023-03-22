6e5f0e38488cd924f5800f12209351785dc9cf9e
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2023-03-21 23:23:12 -0400

    correcao preventDefault

--- main.js
+++ main.js
@@ -4,16 +4,16 @@ const mensagem = document.getElementById('mensagem');
 form.addEventListener('submit', function(e) { //sera disparado quando um evento do tipo 'submit' acontecer no form
     const campoA = parseInt(document.getElementById('campo-a').value); //parseInt converte para numero intero
     const campoB = parseInt(document.getElementById('campo-b').value);
-    e.preventDefault(); // interrompe o envio do formulário
     
     if (campoB <= campoA) {
-    mensagem.textContent = 'Erro! O Valor B deve ser maior que o Valor A!';
-    mensagem.classList.add('erro');
-    mensagem.classList.add('sucesso');
+        mensagem.textContent = 'Erro! O Valor B deve ser maior que o Valor A!';
+        mensagem.classList.add('erro'); //adiciona css mensagem de erro
+        mensagem.classList.remove('sucesso'); //remove css mensagem de erro
     } else {
         mensagem.textContent = 'Formulário enviado com sucesso!';
         mensagem.classList.add('sucesso');
         mensagem.classList.remove('erro');
     }
+    e.preventDefault(); // interrompe o envio do formulário
 });
 console.log(form); //mostrar no console, a constante
