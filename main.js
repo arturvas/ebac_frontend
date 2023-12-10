b78a03de73b52f0f2463152d607b52e86bf23ce7
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2023-12-10 01:02:37 -0400

    melhorado codigo, inserido herancas

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 function Filme(titulo) {
   this.titulo = titulo;
-  this.filmeSelecionado = function () {
+  this.tituloFilme = function () {
     console.log(`Filme selecionado: ${this.titulo}`);
+  };
+}
+
+function NotaIMDB(avaliacao) {
+  this.avaliacao = avaliacao;
+  this.dizNota = function () {
+    console.log(`Nota IMDB: ${this.avaliacao}/10`);
   };
 }
 
@@ -11,26 +18,30 @@ function InfoFilme(
   duracao,
   diretor,
   premios,
-  indicacoes
+  indicacoes,
+  avaliacao
 ) {
-  Filme.call(this, titulo);
   this.anoLancamento = anoLancamento;
   this.duracao = duracao;
   this.diretor = diretor;
   this.premios = premios;
   this.indicacoes = indicacoes;
+  this.avaliacao = avaliacao;
 
+  Filme.call(this, titulo);
+  NotaIMDB.call(this, avaliacao);
+
+  // funcao para transformar minutos em horas e minutos.
   this.formataDuracao = function () {
     let duracaoHoras = Math.floor(this.duracao / 60);
-    let duracaoMinutos = (this.duracao / 60 - duracaoHoras) * 100;
+    let duracaoMinutos = this.duracao % 60;
 
-    if (duracaoMinutos >= 60) {
-      duracaoHoras = duracaoHoras + 1;
-      duracaoMinutos = duracaoMinutos - 60;
+    let resultado = duracaoHoras + "h";
+    // aqui se os minutos resultarem em 0, o mesmo nao sera exibido.
+    if (duracaoMinutos > 0) {
+      resultado += ` ${duracaoMinutos.toFixed(0)}min`;
     }
-
-    const resultado = duracaoHoras + "h" + duracaoMinutos.toFixed(0);
-    console.log(resultado);
+    console.log(`Duração: ${resultado}`);
   };
 }
 
@@ -40,7 +51,8 @@ const filme1 = new InfoFilme(
   207,
   "Akira Kurosawa",
   4,
-  6
+  6,
+  8.6
 );
 
 const filme2 = new InfoFilme(
@@ -49,28 +61,38 @@ const filme2 = new InfoFilme(
   120,
   "Fred Zinnemann",
   33,
-  6
+  6,
+  7.7
 );
+
 const filme3 = new InfoFilme(
   "Rear Window",
   1954,
   112,
   "Alfred Hitchcock",
   5,
-  8
+  8,
+  8.5
 );
+
 const filme4 = new InfoFilme(
   "The Tragedy of Macbeth",
   2021,
   105,
   "Joel Coen",
   2,
-  12
+  12,
+  7.1
 );
 
 console.log(filme1);
+filme1.tituloFilme();
 filme1.formataDuracao();
+filme1.dizNota();
 
+console.log("  ");
+
+console.log(filme2);
+filme2.tituloFilme();
 filme2.formataDuracao();
-filme3.formataDuracao();
-filme4.formataDuracao();
+filme2.dizNota();
